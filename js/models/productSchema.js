0769b565547221bddfefed501ae34efa6717199f
Matías <Villalvama@gmail.com>
2017-09-06 12:39:43 -0300

    Update productSchema.js

---
+++ js/models/productSchema.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+// Mongoose productSchema definition
+productSchema = new mongoose.Schema({
+  uniqueCode: Number,
+  categories: [{
+    uniqueCode: Number,
+    name: String
+  }],
+  name: String,
+  img: [String],
+  description: String,
+  priceSince: Number,
+  variations: [{
+    variationId: Number,
+    variationName: String,
+    variationImg: [String],
+    variationDescription: String,
+    price: Number
+  }]
+});
+
+export module productSchema;
