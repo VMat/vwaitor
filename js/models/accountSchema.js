d9bc35d1ef2435f64cf5a1d9cdabecf1b5cf7eeb
Matías <Villalvama@gmail.com>
2017-09-06 12:45:29 -0300

    Create accountSchema.js

---
+++ js/models/accountSchema.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+
+accountSchema = new mongoose.Schema({ 
+  uniqueCode: Number,
+  tableUniqueCodes: [Number],
+
+    requests: [{
+        uniqueCode: Number,
+        person: {
+            personUniqueCode: Number,
+            name: String,
+            documentNumber: Number
+        },
+        products: [{
+            productUniqueCode: Number,
+            name: String,
+            price: Number
+        }],
+        total: Number
+    }],
+
+    total: Number,
+    open: Boolean,
+    paymentMethod: String
+});
+
+export module accountSchema;
