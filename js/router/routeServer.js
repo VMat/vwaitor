1c5c0ebed91228e72c2ffd20a4ab2cca884ab7fa
Matías <Villalvama@gmail.com>
2017-09-06 18:04:08 -0300

    Update routeServer.js

--- js/router/routeServer.js
+++ js/router/routeServer.js
@@ -1,17 +1,18 @@
-var express = require('express');
-var app = express();
+var routeServer = require('express').Router();
 
 var productApi = require('../rest/productApi');
 var accountApi = require('../rest/accountApi');
 var requestApi = require('../rest/requestApi');
 
-app.get('/', function (req, res){
+routeServer.get('/', function (req, res){
   res.json(200, {msg: 'OK' });
 });
 
 // API REST PRODUCTOS
-.use('/productos', productApi)
+routeServer.use('/productos', productApi);
 // API REST CUENTAS
-.use('/cuentas', accountApi)
+routeServer.use('/cuentas', accountApi);
 // API REST PEDIDOS
-.use('/pedidos', requestApi);
+routeServer.use('/pedidos', requestApi);
+
+module.exports = routeServer;
