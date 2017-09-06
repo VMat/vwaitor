2557829aa74b516c1031ab225ef30250e59f454c
Matías <Villalvama@gmail.com>
2017-09-06 14:31:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,305 +1,81 @@
 var express = require('express');
 var app = express();
-var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
+var storageService = require('./js/service/storageService');
+
 // use it before all route definitions
 app.use(cors({origin: 'http://localhost:8000'}));
-
-// Mongoose productSchema definition
-productSchema = new mongoose.Schema({
-  uniqueCode: Number,
-  categories: [{
-    uniqueCode: Number,
-    name: String
-  }],
-  name: String,
-  img: [String],
-  description: String,
-  priceSince: Number,
-  variations: [{
-    variationId: Number,
-    variationName: String,
-    variationImg: [String],
-    variationDescription: String,
-    price: Number
-  }]
-});
-
-Products = mongoose.model('admin', productSchema, 'Products');
-
-requestSchema = new mongoose.Schema({
-  uniqueCode: Number,
-  person: {
-      personUniqueCode: Number,
-      name: String,
-      documentNumber: Number
-  },
-  products: [{
-    uniqueCode: Number, 
-    categories: [{
-      uniqueCode: Number,
-      name: String
-    }],
-    name: String,
-    img: [String], 
-    description: String, 
-    priceSince: Number,
-    variations: [{
-      variationId: Number, 
-      variationName: String, 
-      variationImg: [String], 
-      variationDescription: String, 
-      price: Number, 
-      amount: Number
-    }], 
-    amount: Number
-  }],
-  total: Number
-});
-
-Requests = mongoose.model('admin1', requestSchema, 'Requests');
-
-/*accountSchema = new mongoose.Schema({
-  uniqueCode: Number,
-  requests:[{
-    uniqueCode: Number,
-    person: {
-        personUniqueCode: Number,
-        name: String,
-        documentNumber: Number
-    },
-    products: [{
-      uniqueCode: Number, 
-      categories: [{
-        uniqueCode: Number,
-        name: String
-      }],
-      name: String,
-      img: [String], 
-      description: String, 
-      priceSince: Number,
-      variations: [{
-        variationId: Number, 
-        variationName: String, 
-        variationImg: [String], 
-        variationDescription: String, 
-        price: Number, 
-        amount: Number
-      }], 
-      amount: Number
-    }],
-    total: Number
-  }]
-});*/
-
-accountSchema = new mongoose.Schema({ 
-  uniqueCode: Number,
-  tableUniqueCodes: [Number],
-
-    requests: [{
-        uniqueCode: Number,
-        person: {
-            personUniqueCode: Number,
-            name: String,
-            documentNumber: Number
-        },
-        products: [{
-            productUniqueCode: Number,
-            name: String,
-            price: Number
-        }],
-        total: Number
-    }],
-
-    total: Number,
-    open: Boolean,
-    paymentMethod: String
-});
-
-Account = mongoose.model('admin2', accountSchema, 'Accounts');
-
-// connect with MONGOLAB
-mongoose.connect(process.env.MONGOLAB_URI, function (error) {
-    if (error) console.error(error);
-    else console.log('mongo connected');
-});
 
 app
   // https://scotch.io/tutorials/use-expressjs-to-get-url-and-post-parameters
   .use(bodyParser.json()) // support json encoded bodies
   .use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
 
-  .get('/api', function (req, res) {
+  .get('/api', function (req, res){
     res.json(200, {msg: 'OK' });
   })
 
   // API REST PRODUCTOS
   
-  .get('/productos', function (req, res) {
-    // http://mongoosejs.com/docs/api.html#query_Query-find
-    Products.find( function (err, todos ){
-      res.json(200, todos);
-    });
+  .get('/productos', function (req, res){
+  
+    storageService.getProducts(req, res);
   })
 
-  .post('/productos', function (req, res) {
-    var todo = new Products( req.body );
-    todo.id = todo._id;
-    // http://mongoosejs.com/docs/api.html#model_Model-save
-    todo.save(function (err) {
-      res.json(200, todo);
-    });
+  .post('/productos', function (req, res){
+  
+    storageService.createProduct(req, res);
   })
 
-  .del('/productos', function (req, res) {
-    // http://mongoosejs.com/docs/api.html#query_Query-remove
-    Products.remove({ completed: true }, function (err ) {
-      res.json(200, {msg: 'OK'});
-    });
+  .del('/productos', function (req, res){
+    storageService.deleteProducts(req ,res);
   })
 
-  .get('/productos/:id', function (req, res) {
-    // http://mongoosejs.com/docs/api.html#model_Model.findById
-    Products.findByUniqueCode( req.params.id, function (err, todo ) {
-      res.json(200, todo);
-    });
+  .get('/productos/:id', function (req, res){
+    storageService.getProduct(req, res);
   })
 
-  .put('/productos/:id', function (req, res) {
-    // http://mongoosejs.com/docs/api.html#model_Model.findById
-    Products.findByUniqueCode( req.params.id, function (err, todo ) {
-      todo.title = req.body.title;
-      todo.completed = req.body.completed;
-      // http://mongoosejs.com/docs/api.html#model_Model-save
-      todo.save( function ( err, todo ){
-        res.json(200, todo);
-      });
-    });
+  .put('/productos/:id', function (req, res){
+    storageService.updateProduct(req,res);
   })
 
-  .del('/productos/:id', function (req, res) {
-    // http://mongoosejs.com/docs/api.html#model_Model.findById
-    Products.findByUniqueCode( req.params.id, function (err, todo ) {
-      // http://mongoosejs.com/docs/api.html#model_Model.remove
-      todo.remove( function ( err, todo ){
-        res.json(200, {msg: 'OK'});
-      });
-    });
+  .del('/productos/:id', function (req, res){
+    storageService.deleteProduct(req, res);
   })
+
 
 // API REST CUENTAS
 
   .get('/cuentas', function(req, res){
-  
-    Accounts.find(function (err, accounts) {
-      res.json(200, accounts);
-    });
+    storageService.getAccounts(req, res);
   })
 
   .get('/cuentas/:id', function(req, res){
-  
-    Accounts.findByUniqueCode( req.params.id, function (err, accounts ) {
-      
-      if(accounts.length > 0){
-        res.json(200, accounts[0]);
-      }
-      else{
-        res.json(200, {});
-      }
-      
-    });
+    storageService.getAccount(req, res);
   })
 
-  .post('/cuentas', function (req, res) {
-  
-    var maxUniqueCode = 0;
-  
-    Accounts.find({}).
-      //where('name.last').equals('Ghost').
-      //where('age').gt(17).lt(66).
-      //where('likes').in(['vaporizing', 'talking']).
-      limit(1).
-      sort('-uniqueCode').
-      select('uniqueCode').
-      exec(function(err,accounts){
-        accounts.map(function (account) {
-          maxUniqueCode = account.uniqueCode;
-      });
-  
-      var newAccount = new Accounts(req.body);
-      newAccount.uniqueCode = maxUniqueCode + 1;
-      newAccount.save(function (err) {
-        res.json(200, newAccount);
-      });
-    });
+  .post('/cuentas', function (req, res){
+    storageService.createAccount(req, res);
   })
 
   .put('/cuentas/:id', function (req, res){
-  
-    var newRequest = new Accounts(req.body);
-         
-    Accounts.findByUniqueCode( req.params.id, function (err, oldAccount ) {
-      oldAccount.products.concat(req.body);
-      oldAccount.save( function ( err, updatedAccount ){
-        res.json(200, updatedAccount);
-      });
-    });
-    
+    storageService.updateAccount(req, res);
   })
 
 // API REST PEDIDOS
 
-  .get('/pedidos', function (req, res) {
-    // http://mongoosejs.com/docs/api.html#query_Query-find
-    Requests.find( function (err, requests ){
-      res.json(200, requests);
-    });
+  .get('/pedidos', function (req, res){
+    storageService.getRequests(req, res);
   })
 
-  .post('/pedidos', function (req, res) {
-  
-    var maxUniqueCode = 0;    
-  
-    Requests.find({}).
-      limit(1).
-      sort('-uniqueCode').
-      select('uniqueCode').
-      exec(function(err,requests){
-        requests.map(function (request) {
-          maxUniqueCode = request.uniqueCode;
-        });
-      });
-  
-    var newRequest = new Requests(req.body);
-    // newRequest.id = newRequest._id;
-    // http://mongoosejs.com/docs/api.html#model_Model-save
-    newRequest.uniqueCode = maxUniqueCode + 1;
-    newRequest.save(function (err) {
-      Accounts.find( function (err, accounts){
-        if(accounts.length>0){
-          var newAccount = accounts[0].requests.push(newRequest);
-        }
-        else{
-          var newAccount = new Accounts({uniqueCode: 1, requests: [newRequest]});
-        }
-        newAccount.save(function (err) {
-        }
-      });
-      res.json(200, newRequest);
-    });
+  .post('/pedidos', function (req, res){
+    storageService.createRequest(req, res)
   })
       
   .put('/pedidos/:id', function (req, res) {
-      // http://mongoosejs.com/docs/api.html#model_Model.findById
-      Requests.findByUniqueCode( req.params.id, function (err, oldRequest ) {
-          oldRequest.products.push(req.body.products);
-          // http://mongoosejs.com/docs/api.html#model_Model-save
-          oldRequest.save( function ( err, updatedRequest ){
-              res.json(200, updatedRequest);
-          });
-      });
+    storageService.updateRequest(req, res);
   });
 
 app.set('port', (process.env.PORT || 5000));
