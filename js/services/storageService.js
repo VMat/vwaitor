23a502500fc2a27fbd80e7361a3e296b9d83a49e
Matías <Villalvama@gmail.com>
2017-09-06 14:40:22 -0300

    Update storageService.js

--- js/services/storageService.js
+++ js/services/storageService.js
@@ -8,9 +8,63 @@ var storageService = (function(){
     
     connect: function(){
       db.connect();
-    }
+    },
+    
+    getProducts: function(req, res){
+      db.getProducts(req, res);
+    },
+    
+    getProduct: function(req, res){
+      db.getProduct(req, res);
+    },
+    
+    createProduct: function(req, res){
+      db.createProduct(req, res);
+    },
+    
+    updateProduct: function(req, res){
+      db.updateProduct(req, res);
+    },
+    
+    deleteProducts: function(req, res){
+      db.deleteProducts(req, res);
+    },
+    
+    deleteProduct: function(req, res){
+      db.deleteProduct(req, res);
+    },
+    
+    getAccounts: function(req, res){
+      db.getAccount(req, res);
+    },
+   
+    getAccounts: function(req, res){
+      db.getAccount(req, res);
+    },
+    
+    createAccount: function(req, res){
+      db.createAccount(req, res);
+    },
+    
+    updateAccount: function(req, res){
+      db.updateAccount(req, res);
+    },
+    
+    getRequests: function(req, res){
+      db.getRequests(req, res);
+    },
+    
+    createRequest: function(req, res){
+      db.createRequest(req, res);
+    },
+    
+    updateRequest: function(req, res){
+      db.updateRequest(req, res);
+    },
     
   };
 
 })();
+
+export module storageService;
 
