60a98b6bcec18f27acf70c33d0347f0facc1f620
Matías <Villalvama@gmail.com>
2017-09-20 15:11:17 -0300

    Update account.js

--- js/models/account.js
+++ js/models/account.js
@@ -3,25 +3,23 @@ var mongoose = require('mongoose');
 accountSchema = new mongoose.Schema({ 
   uniqueCode: Number,
   tableUniqueCodes: [Number],
-
-    requests: [{
-        uniqueCode: Number,
-        person: {
-            personUniqueCode: Number,
-            name: String,
-            documentNumber: Number
-        },
-        products: [{
-            productUniqueCode: Number,
-            name: String,
-            price: Number
-        }],
-        total: Number
-    }],
-
-    total: Number,
-    open: Boolean,
-    paymentMethod: String
+  requests: [{
+      uniqueCode: Number,
+      person: {
+          personUniqueCode: Number,
+          name: String,
+          documentNumber: Number
+      },
+      products: [{
+          productUniqueCode: Number,
+          name: String,
+          price: Number
+      }],
+      total: Number
+  }],
+  total: Number,
+  open: String,
+  paymentMethod: String
 });
 
 Account = mongoose.model('admin2', accountSchema, 'Accounts');
