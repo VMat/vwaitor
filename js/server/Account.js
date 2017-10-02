da1496bafa9e92726d2d19c97c97577564dcbaf5
Matías <Villalvama@gmail.com>
2017-10-02 15:07:15 -0300

    Update and rename server.js to Account.js

---
+++ js/server/Account.js
@@ -0,0 +1,16 @@
+const Account = (function(){
+
+  function oAccount(){}
+  
+  oAccount.prototype = {
+  
+    getAccounts: function(){     
+    }
+  
+  }
+
+  return oAccount;
+
+})();
+
+module.export Account;
