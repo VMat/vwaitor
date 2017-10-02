fc839df62aa0aceb2a556e3da324139777d4242d
Matías <Villalvama@gmail.com>
2017-10-02 16:03:43 -0300

    Update requestService.js

--- js/services/requestService.js
+++ js/services/requestService.js
@@ -6,9 +6,29 @@ const requestService = (function(){
   
   oRequestService.prototype = {
   
-    getRequests: function(){      
-      return storageService.getRequests();
-    }
+    getRequests: function(req, res){      
+      return storageService.getRequests(req, res);
+    },
+    
+    getRequest: function(req, res){
+      return requestService.getRequest(req, res); 
+    },   
+
+    createRequest: function(req, res){
+      return requestService.createRequest(req, res); 
+    },
+
+    updateRequest: function(req, res){
+      return requestService.updateRequest(req, res); 
+    },
+    
+    deleteRequests: function(req, res){
+      return requestService.deleteRequests(req, res);
+    },
+
+    deleteRequest: function(req, res){
+      return requestService.deleteRequest(req, res);
+    }    
   
   }
 
