2f61d05f294e62b48d62f0bb1ac74463fa019dc2
Matías <Villalvama@gmail.com>
2017-10-02 15:55:06 -0300

    Update accountService.js

--- js/services/accountService.js
+++ js/services/accountService.js
@@ -6,11 +6,35 @@ const accountService = (function(){
   
   oAccountService.prototype = {
   
-    getAccounts: function(){      
-      return storageService.getAccounts();
+    getAccounts: function(req, res){      
+      return storageService.getAccounts(req, res);
+    },
+    
+    getAccount: function(req, res){
+      return storageService.getAccount(req, res);
+    },
+    
+    createAccount: function(req, res){
+      return storageService.createAccount(req, res);
+    },
+    
+    updateAccount: function(req, res){
+      return storageService.updateAccount(req, res);
+    },
+    
+    patchAccount: function(req, res){
+      return storageService.patchAccount(req, res);
+    },
+    
+    deleteAccounts function(req, res){
+      return storageService.deleteAccounts(req, res);
+    },
+    
+    deleteAccount: function(req, res){
+      return storageService.deleteAccount(req, res);
     }
   
-  }
+  };
 
   return oAccountService;
 
