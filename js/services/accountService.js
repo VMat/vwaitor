5ff546ae935b323ca0a0ae7a53ce41d9ee442fba
Matías <Villalvama@gmail.com>
2017-10-03 12:42:15 -0300

    Update accountService.js

--- js/services/accountService.js
+++ js/services/accountService.js
@@ -6,32 +6,60 @@ const accountService = (function(){
   
   oAccountService.prototype = {
   
-    getAccounts: function(){      
-      return storageService.getAccounts();
+    getAccounts: function(){
+      return new Promise((resolve, reject)=>{
+        storageService.getAccounts().
+          then(accounts=>resolve(accounts)).
+          catch(error=>reject(error))        
+      })
     },
     
     getAccount: function(id){
-      return storageService.getAccount(id);
+      return new Promise((resolve, reject)=>{
+        storageService.getAccount(id).
+          then(account=>resolve(account)).
+          catch(error=>reject(error))        
+      })
     },
     
     createAccount: function(account){
-      return storageService.createAccount(account);
+      return new Promise((resolve, reject)=>{
+        storageService.createAccount(account).
+          then(newAccount=>resolve(newAccount)).
+          catch(error=>reject(error))        
+      })
     },
     
     updateAccount: function(id, account){
-      return storageService.updateAccount(id, account);
+      return new Promise((resolve, reject)=>{
+        storageService.updateAccount(id, account).
+          then(updatedAccount=>resolve(updatedAccount)).
+          catch(error=>reject(error))        
+      })
     },
     
     patchAccount: function(id, account){
-      return storageService.patchAccount(id, account);
+      return new Promise((resolve, reject)=>{
+        storageService.patchAccount(id, account).
+          then(patchedAccount=>resolve(patchedAccount)).
+          catch(error=>reject(error))        
+      })
     },
     
     deleteAccounts: function(){
-      return storageService.deleteAccounts();
+      return new Promise((resolve, reject)=>{
+        storageService.deleteAccounts().
+          then(deletedAccounts=>resolve(deletedAccounts)).
+          catch(error=>reject(error))        
+      })
     },
     
     deleteAccount: function(id){
-      return storageService.deleteAccount(id);
+      return new Promise((resolve, reject)=>{
+        storageService.deleteAccount(id).
+          then(deletedAccount=>resolve(deletedAccount)).
+          catch(error=>reject(error))        
+      })
     }
   
   };
