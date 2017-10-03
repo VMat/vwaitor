7b47d0906142c9ffc925f50bafd192c69d8cefb8
Matías <Villalvama@gmail.com>
2017-10-03 12:47:24 -0300

    Update requestService.js

--- js/services/requestService.js
+++ js/services/requestService.js
@@ -6,28 +6,52 @@ const requestService = (function(){
   
   oRequestService.prototype = {
   
-    getRequests: function(req, res){      
-      return storageService.getRequests(req, res);
+    getRequests: function(){
+      return new Promise((resolve, reject)=>{
+        storageService.getRequests().
+          then(requests=>resolve(requests)).
+          catch(error=>reject(error))        
+      })
     },
     
-    getRequest: function(req, res){
-      return requestService.getRequest(req, res); 
+    getRequest: function(id){
+      return new Promise((resolve, reject)=>{
+        requestService.getRequest(id).
+          then(request=>resolve(request)).
+          catch(error=>reject(error))        
+      })
     },   
 
-    createRequest: function(req, res){
-      return requestService.createRequest(req, res); 
+    createRequest: function(request){
+      return new Promise((resolve, reject)=>{
+        requestService.createRequest(request).
+          then(newRequest=>resolve(newRequest)).
+          catch(error=>reject(error))        
+      })
     },
 
-    updateRequest: function(req, res){
-      return requestService.updateRequest(req, res); 
+    updateRequest: function(id, request){
+      return new Promise((resolve, reject)=>{
+        requestService.updateRequest(id, request).
+          then(updatedRequest=>resolve(updatedRequest)).
+          catch(error=>reject(error))        
+      })
     },
     
-    deleteRequests: function(req, res){
-      return requestService.deleteRequests(req, res);
+    deleteRequests: function(){
+      return new Promise((resolve, reject)=>{
+        requestService.deleteRequests().
+          then(deletedRequests=>resolve(deletedRequests)).
+          catch(error=>reject(error))        
+      })
     },
 
-    deleteRequest: function(req, res){
-      return requestService.deleteRequest(req, res);
+    deleteRequest: function(id){
+      return new Promise((resolve, reject)=>{
+        requestService.deleteRequest(id).
+          then(deletedRequest=>resolve(deletedRequest)).
+          catch(error=>reject(error))        
+      })
     }    
   
   }
