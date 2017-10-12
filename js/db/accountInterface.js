d104fade3355d4f1ecff4dba911ac4f9bfb44ce2
Matías <Villalvama@gmail.com>
2017-10-12 15:35:59 -0300

    Create accountInterface.js

---
+++ js/db/accountInterface.js
@@ -0,0 +1,13 @@
+let AccountInterface = (function(){
+
+  function oAccountInterface(){};
+  
+  oAccountInterface.prototype = {};
+  
+  return oAccountInterface;
+
+})();
+
+let oAccountInterface = new AccountInterface();
+
+module.exports = oAccountInterface;
