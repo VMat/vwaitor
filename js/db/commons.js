26f152a23999a1867f56451a7b350d22b9057d03
Matías <Villalvama@gmail.com>
2017-10-12 16:53:10 -0300

    Create commons.js

---
+++ js/db/commons.js
@@ -0,0 +1,19 @@
+let Commons = (function(){
+
+  function oCommons(){};
+  
+  oCommons.prototype = {
+    
+    getNextUniqueCode: function(Collection){
+    
+    }
+  
+  };
+  
+  return oCommons;
+
+})();
+
+const oCommons = new Commons();
+
+module.exports = oCommons;
