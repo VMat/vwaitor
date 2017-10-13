7b8dd3e92ca79902dc3cb07639e2c672bbf4c467
Matías <Villalvama@gmail.com>
2017-10-13 14:45:53 -0300

    Update commons.js

--- js/db/commons.js
+++ js/db/commons.js
@@ -5,18 +5,21 @@ let Commons = (function(){
   oCommons.prototype = {
     
     getNextUniqueCode: function(Collection,fn){
+      
+      let nextUniqueCode = null;
+      
       Collection.find({}).
         limit(1).
         sort('-uniqueCode').
         select('uniqueCode').
         exec((err,doc)=>{
           if(doc.length>0){
-            fn(doc[0].uniqueCode + 1)
+            nextUniqueCode = doc[0].uniqueCode + 1;
           }
           else{
-            fn(1);
+            nextUniqueCode = 1;
           }
-        });
+        }).then(()=>{fn(nextUniqueCode)});
     }
   
   };
